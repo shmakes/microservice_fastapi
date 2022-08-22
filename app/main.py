dfba25c2e0db80e3095562ea04036dffcd6e8f58
Steve Schmechel <shmakes@gmail.com>
2022-08-22 18:10:18 -0500

    Add AWS lib and sample S3 code

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import boto3
 
 app = FastAPI()
 
@@ -8,6 +9,19 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/{bucket}")
+def read_item(bucket: str = None):
+    s3 = boto3.resource(
+        service_name='s3',
+        aws_access_key_id='aaa',
+        aws_secret_access_key='bbb',
+        endpoint_url='http://localhost:4566',
+    )
+
+    my_bucket = s3.Bucket(bucket)
+
+    fileList = []
+    for my_bucket_object in my_bucket.objects.all():
+        fileList.append(my_bucket_object.key)
+
+    return {"bucket_name": bucket, "items": fileList}
